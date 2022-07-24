5d9afe4cd1b7fd85480e532f8834de65a5bb9935
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-07-24 10:35:27 -0400

    Create masterychanger.lua

---
+++ rbxl/scripts/masterychanger.lua
@@ -0,0 +1,19 @@
+game:Players.LocalPlayer:Kick("You've been banned for being retarded LOL")
+
+local gmt = getrawmetatable(game)
+
+local oldindex = gmt.__index
+local oldnamecall gmt.__namecall
+
+local mod = require(game:GetService("ReplicatedStorage").Modules.Global["Random_Clan_Picker"])
+
+gmt = hookmetamethod(game, "index", function(i, v)
+if tostring(i) == "Spins" then
+
+i.Value = 9e9
+
+end
+
+return oldindex(i, v)
+
+end)
