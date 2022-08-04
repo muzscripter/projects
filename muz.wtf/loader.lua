8336c9d546d91bfaf150590e4ecc832a689bb472
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-08-04 18:49:09 -0400

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -1,9 +1,8 @@
-_G.wl_key="31b364e6939be96662e87594bc4b3401ea703a14"
-
 local supported_games = {
     [3226555017] = "https://scripts.luawl.com/11629/scp:siteroleplay.lua",
     [9551640993] = "https://scripts.luawl.com/11709/miningsim.lua",
-    [6152116144] = "https://scripts.luawl.com/11631/projectslayers.lua"
+    [6152116144] = "https://scripts.luawl.com/11631/projectslayers.lua",
+    [8884334497] = "https://scripts.luawl.com/11774/MiningClickerSimulator.lua"
 }
 
 if supported_games[game.PlaceId] then
