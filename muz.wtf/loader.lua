5ea8391fd5c52fadfe5785601e33a9f5ee4b27e5
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-08-03 21:42:51 -0400

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -1,57 +1,9 @@
-getgenv().access_key = "1FYdwfN0by15$&VeLs46V*mm"
-
-local client = game.Players.LocalPlayer
-
-if getgenv().access_key == '1FYdwfN0by15$&VeLs46V*mm' then
-    print("Key is succesful")
-else
-    game.Players.LocalPlayer:Kick("Wrong key join the discord server | https://discord.gg/fKuzp8YDmT")
-end
-
-
-
-local blacklisted_users = {
-    ['kaos7789'] = true, 
-}
-
-if blacklisted_users[game.Players.LocalPlayer.Name] then 
-    game.Players.LocalPlayer:Kick("Blacklisted from using our scripts")
-end
-
-repeat wait() until game:IsLoaded()
-
 local supported_games = {
-    [3226555017] = {
-        ["Name"] = "SCP: Site Roleplay",
-        ["Function"] = function()
-            local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-            print(GameName .. " Script Loaded")
-        end
-    },
-    [6152116144] = {
-        ["Name"] = "Project Slayers",
-        ["Function"] = function()
-            local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-            print(GameName .. " Script Loaded")
-        end
-    },
-    [5780309044] = {
-        ["Name"] = "A Sakura Stand",
-        ["Function"] = function()
-            local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-            print(GameName .. " Script Loaded")
-        end
-    }
-
+    [3226555017] = "https://scripts.luawl.com/11629/scp:siteroleplay.lua",
+    [9551640993] = "https://scripts.luawl.com/11709/miningsim.lua",
+    [6152116144] = "https://scripts.luawl.com/11631/projectslayers.lua"
 }
 
 if supported_games[game.PlaceId] then
-    pcall(function() supported_games[game.PlaceId].Function() end)
-    local d
-    local f = pcall(function() d = game:HttpGet("https://raw.githubusercontent.com/muzscripter/projects/main/muz.wtf/scripts/"..game.PlaceId..".lua") end)
-    if f == true then
-        loadstring(d)()
-    end
-else
-    game.Players.LocalPlayer:Kick("muz.wtf | game not supported" .. "https://discord.gg/fKuzp8YDmT")
+    pcall(function() return loadstring(supported_games[game.PlaceId])() end)
 end
