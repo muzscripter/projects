3cd68b3977e5dd3a511a67dfbd4c5595a35e7d28
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-08-13 23:27:45 -0400

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -1,12 +1,18 @@
 local supported_games = {
-    [3226555017] = "https://scripts.luawl.com/11629/scp:siteroleplay.lua",
-    [9551640993] = "https://scripts.luawl.com/11709/miningsim.lua",
-    [6152116144] = "https://scripts.luawl.com/11631/projectslayers.lua",
-    [8884334497] = "https://scripts.luawl.com/11774/MiningClickerSimulator.lua",
-    [9845465117] = "https://scripts.luawl.com/11901/aureborn.lua",
-    [8534845015] = "https://scripts.luawl.com/11927/sakurastands.lua"
+    [3226555017] = "https://scripts.luawl.com/11629/scp:siteroleplay.lua";
+    [9551640993] = "https://scripts.luawl.com/11709/miningsim.lua";
+    [6152116144] = "https://scripts.luawl.com/11631/projectslayers.lua";
+    [8884334497] = "https://scripts.luawl.com/11774/MiningClickerSimulator.lua";
+    [9845465117] = "https://scripts.luawl.com/11901/aureborn.lua";
+    [8534845015] = "https://scripts.luawl.com/11927/sakurastands.lua";
 }
 
 if supported_games[game.PlaceId] then
-    pcall(function() return loadstring(game:HttpGet(supported_games[game.PlaceId]))() end)
-end
+    messagebox("Thanks for using muz hub if you have any issues please DM 'c0Ve4zYu2aIoD$F3R63JSlBVnF' " , "MUZ.WTF | HUB" , 0);
+
+    pcall(function() return loadstring(game:HttpGet(supported_games[game.PlaceId]))() end);
+    
+    else
+        game.Players.LocalPlayer:Kick("Game isn't supported")
+end;
+    
