c272f1d5fd344d1cd55058f0acedc91fe8b6cf59
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-09-04 23:50:57 -0400

    Update loader.lua

--- muz.community/loader.lua
+++ muz.community/loader.lua
@@ -8,13 +8,8 @@ local game_https = {
 
 local CoreGui = game:GetService("StarterGui")
 
-if getgenv().comkey[keys] then
+if keys[comkey] then
     if game_https[game.PlaceId] then
-
         pcall(function() return loadstring(game:HttpGet(game_https[game.PlaceId]))() end);
-
-        CoreGui:SetCore("SendNotification", {Title = "muz.community"; Text = "Join the discord server | https://discord.gg/QBdvJ5QymP"; Duration = 60; Button1 = "Ok"; })
-    else
-        game.Players.LocalPlayer:Kick("Wrong key or game isn't supported")
     end
-end;
+end
