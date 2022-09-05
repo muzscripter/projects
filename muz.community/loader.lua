53886257f5ce478003f02e55eee42d0040565915
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-09-05 17:35:15 -0400

    Update loader.lua

--- muz.community/loader.lua
+++ muz.community/loader.lua
@@ -1,15 +1,30 @@
-local keys = {
-    ["release"] = true
+local _keys = {
+    ["release"] = false,
+    ["1iLliLLLi_10X@0+##)@"] = true
 }
 
 local game_https = {
     [6152116144] = "https://raw.githubusercontent.com/muzscripter/projects/main/muz.community/scripts/projectslayers.lua";
 }
 
-local CoreGui = game:GetService("StarterGui")
-
-if keys[comkey] then
-    if game_https[game.PlaceId] then
-        pcall(function() return loadstring(game:HttpGet(game_https[game.PlaceId]))() end);
+local function _checkKey()
+    if _keys[comkey] then
+        return true
+    else
+        return false
     end
 end
+
+
+function check_key()
+    if _checkKey() == true then
+        pcall(function() return loadstring(game:HttpGet(game_https[game.PlaceId]))() end);
+    elseif _checkKey() == false then
+        game.Players.LocalPlayer:Kick("Wrong key or game isn't supported join the discord ") 
+
+        CoreGui:SetCore("SendNotification", {Title = "muz.community"; Text = "Join the discord server | https://discord.gg/QBdvJ5QymP"; Duration = 60; Button1 = "Ok"; })
+    end
+end
+
+check_key()
+
