726123c40aba5e3aa80c6356106a109558ac8e90
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-11-10 17:23:44 -0500

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -1,6 +1,7 @@
 local supported_games = {
     [11468159863] = "https://scripts.luawl.com/11631/projectslayersv2.lua";
     [6152116144] = "https://scripts.luawl.com/13428/projectslayersv1.lua";
+    [10636616861] = "https://scripts.luawl.com/13431/dungeon.lua";
 }
 
 if supported_games[game.PlaceId] then
