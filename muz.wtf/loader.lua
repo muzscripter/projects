8016ca0ed3c22b4a6af0dc0ee5c0e02d46467da5
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-11-11 17:55:53 -0500

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -1,13 +1,13 @@
 local supported_games = {
     [11468159863] = "https://scripts.luawl.com/11631/projectslayersv2.lua";
-    [6152116144] = "https://scripts.luawl.com/13428/projectslayersv1.lua";
+    [6152116144] = "https://scripts.luawl.com/13428/projectslayers_world1.lua";
     [10636616861] = "https://scripts.luawl.com/13431/dungeon.lua";
+    [11468075017] = "https://scripts.luawl.com/13455/hub_dungeon.lua";
 }
 
 if supported_games[game.PlaceId] then
-    pcall(function() return loadstring(game:HttpGet(supported_games[game.PlaceId]))() end);
-    
+   pcall(function() return loadstring(game:HttpGet(supported_games[game.PlaceId]))() end);
     else
-        game.Players.LocalPlayer:Kick("Game isn't supported")
+   game.Players.LocalPlayer:Kick("Game isn't supported")
 end;
     
