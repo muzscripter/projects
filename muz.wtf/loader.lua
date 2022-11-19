c561798b37d595041700919fd2e6b412d1fa81c9
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-11-19 10:14:57 -0500

    Update loader.lua

--- muz.wtf/loader.lua
+++ muz.wtf/loader.lua
@@ -5,7 +5,6 @@ end
 local supported_games = {
     [11468159863] = "https://scripts.luawl.com/11631/projectslayersv2.lua";
     [6152116144] = "https://scripts.luawl.com/13428/projectslayers_world1.lua";
-    [10636616861] = "https://scripts.luawl.com/13431/dungeon.lua";
     [11468075017] = "https://scripts.luawl.com/13455/hub_dungeon.lua";
 }
 
