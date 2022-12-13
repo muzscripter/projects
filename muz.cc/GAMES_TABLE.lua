c78f70484e9dcfcb499942ee62f09e71f80608a1
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-12-13 08:42:55 -0500

    Update GAMES_TABLE.lua

--- muz.cc/GAMES_TABLE.lua
+++ muz.cc/GAMES_TABLE.lua
@@ -1,3 +1,5 @@
 local GAMES = {
-  ['4483381587'] = ''
+  ['4483381587'] = 'https://raw.githubusercontent.com/muzscripter/projects/main/muz.cc/scripts/a%20literaly%20baseplate.lua'
 }
+
+return GAMES
