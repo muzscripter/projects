d413247b1b5d9f33a179bc8b5ee70812d7c32cb3
muzscripter <109730545+muzscripter@users.noreply.github.com>
2022-12-19 17:02:13 -0500

    Update loader.lua

--- best hub/loader.lua
+++ best hub/loader.lua
@@ -1,6 +1,6 @@
 
 if game.PlaceId == 7622046644 then 
- print('Hello World')
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/muzscripter/projects/main/best%20hub/monsterghoul.lua'))()
   else
   print('Trash')
   end
