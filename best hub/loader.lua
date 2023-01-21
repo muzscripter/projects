346669e05e27d8458c584e6e8c5f15a4e89b59b6
muzscripter <109730545+muzscripter@users.noreply.github.com>
2023-01-21 15:34:42 -0500

    Delete best hub directory

--- best hub/loader.lua
+++
@@ -1,7 +0,0 @@
-
-if game.PlaceId == 7622046644 then 
- loadstring(game:HttpGet('https://raw.githubusercontent.com/muzscripter/projects/main/best%20hub/monsterghoul.lua'))()
-  else
-  print('Trash')
-  end
-end
