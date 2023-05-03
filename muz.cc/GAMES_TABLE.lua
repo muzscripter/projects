293dff866c8373b1cb71420fb4dc904edd9905fa
muzscripter <109730545+muzscripter@users.noreply.github.com>
2023-05-03 09:12:04 -0400

    Delete muz.cc directory

--- muz.cc/GAMES_TABLE.lua
+++
@@ -1,7 +0,0 @@
-local GAMES = {
-  [4483381587] = 'https://raw.githubusercontent.com/muzscripter/projects/main/muz.cc/scripts/a%20literaly%20baseplate.lua';
-  [11468159863] = 'https://raw.githubusercontent.com/muzscripter/projects/main/muz.cc/scripts/world2pjs.lua';
-  [6152116144] = 'https://raw.githubusercontent.com/muzscripter/projects/main/muz.cc/scripts/world1pjs.lua';
-}
-
-return GAMES
